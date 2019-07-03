462c82e705c4416d78f7c0e428b78921a2ba4a05
Md. Tanvir Hossain <tanvir.hossain20@northsouth.edu>
2019-07-04 04:29:50 +0600

    Login interface for Admin and buyer
    Login interface for Admins and buyers.

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Shopmart
                 </div>
 
                 <div class="links">
