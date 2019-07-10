525eb1d55e7def7cc0778f29176a09ab9ced263e
Md. Tanvir Hossain <tanvir.hossain20@northsouth.edu>
2019-07-10 19:41:36 +0600

    Branding Logo

--- Shopmart Shopping Center/resources/views/welcome.blade.php
+++ Shopmart Shopping Center/resources/views/welcome.blade.php
@@ -83,7 +83,7 @@
     
     <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
       <li class="nav-item">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/home">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="('#')">Contact</a>
